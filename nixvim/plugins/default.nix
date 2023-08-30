11aac2bbe11e6489b471e216be5fe638b8dce5f2
varchar <v.richard@protonmail.com>
2023-08-30 11:39:03 +0200

    chore: move telescope conf

--- nixvim/plugins/default.nix
+++ nixvim/plugins/default.nix
@@ -1,5 +1,7 @@
 {
   imports = [
+    ./editor
+
     ./ui.nix
   ];
 }
--- nixvim/plugins/ui.nix
+++ nixvim/plugins/ui.nix
@@ -2,30 +2,6 @@
   plugins.noice.enable = true;
   plugins.which-key.enable = true;
   plugins.indent-blankline.enable = true;
-  plugins.telescope = {
-    enable = true;
-    keymaps = {
-      "<leader>fg" = "live_grep";
-      "<leader>ff" = "find_files";
-      "<leader><space>" = "find_files";
-    };
-    extraOptions = {
-      defaults = {
-        prompt_prefix = " ";
-        selection_caret = " ";
-        mappings = {
-          n = {
-            q = {
-              __raw = ''
-              function(...)
-                return require("telescope.actions").close(...)
-              end'';
-            };
-          };
-        };
-      };
-    };
-  };
   plugins.lualine = {
     enable = true;
     globalstatus = true;
