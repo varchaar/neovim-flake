c2fd201def01791d73f287ba0a8fb323ff1b3e9c
varchar <v.richard@protonmail.com>
2023-11-06 09:59:24 +0100

    feat(lsp): add eslint language server

--- nixvim/plugins/lsp/default.nix
+++ nixvim/plugins/lsp/default.nix
@@ -6,6 +6,14 @@
       rust-analyzer.enable = true;
       nil_ls.enable = true;
       tsserver.enable = true;
+      eslint.enable = true;
+      eslint.onAttach.function = ''
+        if client.name == "eslint" then
+          client.server_capabilities.documentFormattingProvider = true
+        elseif client.name == "tsserver" then
+          client.server_capabilities.documentFormattingProvider = false
+        end
+      '';
     };
   };
   extraPackages = with pkgs; [
