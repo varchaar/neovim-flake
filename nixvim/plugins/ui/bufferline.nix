fd7bf6fc3a356a6ba1bbb2e9f0657157be0fe5c6
varchar <v.richard@protonmail.com>
2024-02-13 11:36:25 +0100

    feat(ui): add bufferline management keymap

--- nixvim/plugins/ui/bufferline.nix
+++ nixvim/plugins/ui/bufferline.nix
@@ -3,8 +3,8 @@
     enable = true;
     alwaysShowBufferline = false;
     diagnostics = "nvim_lsp";
-    closeCommand = ''function(n) require("mini.bufremove").delete(n, false) end'';
-    rightMouseCommand = ''function(n) require("mini.bufremove").delete(n, false) end'';
+    closeCommand = {__raw = ''function(n) require("mini.bufremove").delete(n, false) end'';};
+    rightMouseCommand = {__raw = ''function(n) require("mini.bufremove").delete(n, false) end'';};
     offsets = [
       {
         filetype = "neo-tree";
@@ -61,6 +61,37 @@
       action = ''function() require("mini.bufremove").delete(0, true) end'';
       lua = true;
       options.desc = "Delete Buffer (Force)";
+    }
+
+    {
+      key = "<leader>bp";
+      mode = "n";
+      action = ''<Cmd>BufferLineTogglePin<CR>'';
+      options.desc = "Toggle pin";
+    }
+    {
+      key = "<leader>bP";
+      mode = "n";
+      action = ''<Cmd>BufferLineGroupClose ungrouped<CR>'';
+      options.desc = "Delete non-pinned buffers";
+    }
+    {
+      key = "<leader>bo";
+      mode = "n";
+      action = ''<Cmd>BufferLineCloseOthers<CR>'';
+      options.desc = "Delete other buffers";
+    }
+    {
+      key = "<leader>br";
+      mode = "n";
+      action = ''<Cmd>BufferLineCloseRight<CR>'';
+      options.desc = "Delete buffers to the right";
+    }
+    {
+      key = "<leader>bl";
+      mode = "n";
+      action = ''<Cmd>BufferLineCloseLeft<CR>'';
+      options.desc = "Delete buffers to the left";
     }
   ];
 }
