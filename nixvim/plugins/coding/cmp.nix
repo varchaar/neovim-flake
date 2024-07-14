c783b4c8e990a3e2e710afe11f72b360d53173f8
varchar <v.richard@protonmail.com>
2024-07-14 18:53:19 +0200

    feat(coding/cmp): update cmp binding

--- nixvim/plugins/coding/cmp.nix
+++ nixvim/plugins/coding/cmp.nix
@@ -6,54 +6,21 @@
       snippet.expand = "luasnip";
       mappingPresets = ["insert" "cmdline"];
       mapping = {
-        "<C-b>" = ''cmp.mapping.scroll_docs(-4)'';
-        "<C-f>" = ''cmp.mapping.scroll_docs(4)'';
-        "<C-Space>" = ''
-          cmp.mapping.complete({
-            config = {
-              sources = {
-                { name = "nvim_lsp" },
-                { name = "luasnip" },
-                { name = "path" },
-                { name = "buffer" },
-              }
-            }
-          })
-        '';
-        "<C-e>" = "cmp.mapping.abort()";
-        "<CR>" = "cmp.mapping.confirm({ select = true })";
-        "<Tab>" = ''
-          cmp.mapping(
-            function(fallback)
-              if luasnip.jumpable(1) then
-                luasnip.jump(1)
-              elseif cmp.visible() then
-                cmp.select_next_item()
-              elseif luasnip.expandable() then
-                luasnip.expand()
-              elseif luasnip.expand_or_jumpable() then
-                luasnip.expand_or_jump()
-              elseif check_backspace() then
-                fallback()
-              else
-                fallback()
-            end
-          end
-          , {'i', 's'})
-        '';
-        "<S-Tab>" = ''
-          cmp.mapping(
-            function(fallback)
-              if cmp.visible() then
-                cmp.select_prev_item()
-              elseif luasnip.jumpable(-1) then
-                luasnip.jump(-1)
-              else
-                fallback()
-              end
-            end
-          , {'i', 's'})
-        '';
+        "<C-n>" = ''cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert })'';
+          "<C-p>" = ''cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert })'';
+          "<C-b>" = ''cmp.mapping.scroll_docs(-4)'';
+          "<C-f>" = ''cmp.mapping.scroll_docs(4)'';
+          "<C-Space>" = ''cmp.mapping.complete()'';
+          "<C-e>" = ''cmp.mapping.abort()'';
+          "<CR>" = ''cmp.mapping.confirm({ select = true })''; # Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items
+          "<S-CR>" = ''cmp.mapping.confirm({
+            behavior = cmp.ConfirmBehavior.Replace,
+            select = true,
+          })''; # Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+          "<C-CR>" = ''function(fallback)
+            cmp.abort()
+            fallback()
+          end'';
       };
 
       formatting = {
