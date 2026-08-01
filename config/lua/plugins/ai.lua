90160b0246bf3c90caf7f2ed0a12bdf2ae9e970c
varchar <v.richard@protonmail.com>
2026-08-01 17:05:02 +0200

    chore(ai): drop cursortab integration

--- config/lua/plugins/ai.lua
+++ config/lua/plugins/ai.lua
@@ -1,17 +1,1 @@
-return {
-  {
-    "cursortab/cursortab.nvim",
-    -- version = "*",  -- Use latest tagged version for more stability
-    lazy = false, -- The server is already lazy loaded
-    build = "cd server && go build",
-    config = function()
-      require("cursortab").setup({
-        provider = {
-          type = "zeta-2",
-          model = "zed-industries_zeta-2",
-          url = "http://192.168.100.204:8080",
-        },
-      })
-    end,
-  },
-}
+return {}
--- plugins.nix
+++ plugins.nix
@@ -20,28 +20,6 @@ let
     src = inputs.chameleon-nvim;
   };
 
-  # Build the Go server component separately
-  cursortab-server = pkgs.buildGoModule {
-    pname = "cursortab-server";
-    version = "unstable";
-    src = "${inputs.cursortab-nvim}/server";
-    vendorHash = "sha256-IvJw+89eZ5Ghppjt0KT9IRL8XPyU6XbiAYL3axQO6u4=";
-  };
-
-  cursortab-nvim = pkgs.vimUtils.buildVimPlugin {
-    pname = "cursortab.nvim";
-    version = "unstable";
-    src = inputs.cursortab-nvim;
-    # Patch to use direct Nix store path for server binary
-    # (nixvim bundles plugins into plugin-pack, losing the server/ directory)
-    postPatch = ''
-      substituteInPlace lua/cursortab/daemon.lua \
-        --replace-fail 'local binary_path = plugin_dir .. "/server/" .. binary_name' \
-                       'local binary_path = "${cursortab-server}/bin/cursortab"'
-    '';
-    nvimRequireCheck = [ "cursortab" ];
-  };
-
   mkEntryFromDrv =
     drv:
     if lib.isDerivation drv then
@@ -120,6 +98,8 @@ let
     lazydev-nvim
     grug-far-nvim
     git-blame-nvim
+    SchemaStore-nvim
+    codesettings-nvim
     {
       name = "LuaSnip";
       path = luasnip;
@@ -140,10 +120,6 @@ let
     {
       name = "chameleon.nvim";
       path = chameleon-nvim;
-    }
-    {
-      name = "cursortab.nvim";
-      path = cursortab-nvim;
     }
     {
       name = "mini.ai";
